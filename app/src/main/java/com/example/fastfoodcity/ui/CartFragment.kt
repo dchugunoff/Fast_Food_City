105139570b008df22c3a51974918f11ba3fe22a6
dchugunoff <yolvissplay@gmail.com>
2023-04-06 19:56:50 +0300

    Добавил dialogAllert для очистки корзины

--- app/src/main/java/com/example/fastfoodcity/ui/CartFragment.kt
+++ app/src/main/java/com/example/fastfoodcity/ui/CartFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.example.fastfoodcity.R
 import com.example.fastfoodcity.adapter.CartAdapter
 import com.example.fastfoodcity.databinding.FragmentCartBinding
 import com.example.fastfoodcity.model.CartViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class CartFragment : Fragment() {
 
@@ -36,6 +38,38 @@ class CartFragment : Fragment() {
         // Устанавливаем адаптер recyclerView
         binding.cartRecyclerView.adapter = cartAdapter
 
+//        // Наблюдение за изменениями элементов корзины LiveData и обновление адаптера
+//        cartViewModel.cartItem.observe(viewLifecycleOwner) { cartItems ->
+//            cartAdapter.cartItems = cartItems
+//            isEmpty()
+//            cartAdapter.notifyDataSetChanged()
+//        }
+//
+//        cartViewModel.summaryCost.observe(viewLifecycleOwner) { summaryCost ->
+//            binding.tvTotalCost.text = resources.getString(R.string.price_with_currency, summaryCost)
+//            binding.paymentButton.text = resources.getString(R.string.text_payment_button, summaryCost)
+//        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.clearButton.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(resources.getString(R.string.title_clear_dialog))
+                .setMessage(resources.getString(R.string.supporting_text_clear_dialog))
+                .setNeutralButton(resources.getString(R.string.cancel)) { dialog, which ->
+
+                }
+                .setPositiveButton(resources.getString(R.string.clear_tv_button)) { dialog, which ->
+                    cartViewModel.clearCart()
+                    isEmpty()
+                }
+                .show()
+        }
+
         // Наблюдение за изменениями элементов корзины LiveData и обновление адаптера
         cartViewModel.cartItem.observe(viewLifecycleOwner) { cartItems ->
             cartAdapter.cartItems = cartItems
@@ -47,13 +81,6 @@ class CartFragment : Fragment() {
             binding.tvTotalCost.text = resources.getString(R.string.price_with_currency, summaryCost)
             binding.paymentButton.text = resources.getString(R.string.text_payment_button, summaryCost)
         }
-
-        binding.clearButton.setOnClickListener {
-            cartViewModel.clearCart()
-            isEmpty()
-        }
-        isEmpty()
-        return binding.root
     }
 
 
